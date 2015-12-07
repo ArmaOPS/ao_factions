df06934f56b0acaac61de65e8ada5f9635bd72b4
SuperOPS <operations@armaops.io>
2015-12-06 17:32:58 -0700

    First Commit
    First Commit of the new Pack

---
+++ Addons/ao_main/ao_main/config.cpp
@@ -0,0 +1,77 @@
+////////////////////////////////////////////////////////////////////
+//DeRap: Produced from mikero's Dos Tools Dll version 5.24
+//Produced on Wed Dec 02 00:45:23 2015 : Created on Wed Dec 02 00:45:23 2015
+//http://dev-heaven.net/projects/list_files/mikero-pbodll
+////////////////////////////////////////////////////////////////////
+
+#define _ARMA_
+
+//Class ao_main : config.bin{
+class CfgPatches
+{
+	class AO_main
+	{
+		units[] = {};
+		weapons[] = {};
+		requiredVersion = 1.32;
+		requiredAddons[] = {};
+		author[] = {"kOepi"};
+		authorUrl = "armaops.io";
+	};
+};
+class CfgFactionClasses
+{
+	class AO_BDF
+	{
+		displayName = "Bornholm Defence Forces @AO";
+		author = "kOepi";
+		icon = "";
+		priority = 1;
+		side = 2;
+	};
+	class AO_USMC
+	{
+		displayName = "US Marine Corps Infantry @AO";
+		author = "kOepi";
+		icon = "";
+		priority = 1;
+		side = 1;
+	};
+	class AO_USA
+	{
+	displayName = "[AO] US Army 90s EU";
+		author = "OPS";
+		icon = "";
+		priority = 1;
+		side = 1;
+	};
+};
+class CfgVehicleClasses
+{
+	class AO_BDF_Militia
+	{
+		displayName = "The Great Militia";
+	};
+	class AO_BDF_UrbRes
+	{
+		displayName = "Urban Resistance";
+	};
+	class AO_USMC_D_HQ
+	{
+		displayName = "Platoon HQ desert";
+	};
+	class AO_USMC_D_RSquad
+	{
+		displayName = "Rifle Squad desert";
+	};
+	class AO_USA_BDU_INF
+	{
+		displayName = "US ARMY 1990s EU"
+	}:
+};
+class cfgMods
+{
+	author = "76561197967665515";
+	timepacked = "1449045923";
+};
+//};
---
+++ mod.cpp
@@ -0,0 +1,6 @@
+name = "Arma-Ops Factions";
+actionName = "Website";
+action = "armaops.io";
+tooltipOwned = "AO Owned";
+overview = "Faction Mod for the AO server";
+author = "AO Team";
