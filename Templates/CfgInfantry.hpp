9ff64f4deda89001e6567c825aed6401b25c2bcc
kOepi123 <ickekoep@hotmail.com>
2015-12-11 04:05:26 +0100

    template

---
+++ Templates/CfgInfantry.hpp
@@ -0,0 +1,403 @@
+	class LOP_TKA_Infantry_SL;
+	class LOP_TKA_Infantry_MG;
+	class LOP_TKA_Infantry_Rifleman;
+	class LOP_TKA_Infantry_Rifleman;
+	class LOP_TKA_Infantry_GL;
+	class LOP_TKA_Infantry_Marksman;
+	class LOP_TKA_Infantry_AT;
+	
+	class LOP_TKA_Infantry_TL;
+	class LOP_TKA_Infantry_MG;
+	class LOP_TKA_Infantry_MG;
+	class LOP_TKA_Infantry_Marksman;
+	
+	class AO_Taliban_RSquad_SL : LOP_TKA_Infantry_SL {
+		_generalMacro = "AO_Taliban_RSquad_SL";
+		author = $STR_LOP_FULL_NAME;
+		
+		scope = 2;
+		side = 1;
+	
+		faction = "AO_Taliban";
+		vehicleClass = "AO_Taliban_RSquad";
+		
+		displayName = "Cell Leader";
+		
+		uniformClass = "";
+		
+		Items[] = 	{};
+		RespawnItems[] = {};
+		
+		weapons[] = {};
+		respawnWeapons[] = {};
+		
+		magazines[] = 	{
+						};
+		respawnMagazines[] = {};
+		
+		linkedItems[] = {
+						};
+		respawnLinkedItems[] = {};
+		
+		backpack = "";
+	};
+	class AO_Taliban_RSquad_AR : LOP_TKA_Infantry_MG {
+		_generalMacro = "AO_Taliban_RSquad_AR";
+		author = $STR_LOP_FULL_NAME;
+		
+		scope = 2;
+		side = 1;
+	
+		faction = "AO_Taliban";
+		vehicleClass = "AO_Taliban_RSquad";
+		
+		displayName = "";
+		
+				uniformClass = "";
+		
+		Items[] = 	{};
+		RespawnItems[] = {};
+		
+		weapons[] = {};
+		respawnWeapons[] = {};
+		
+		magazines[] = 	{
+						};
+		respawnMagazines[] = {};
+		
+		linkedItems[] = {
+						};
+		respawnLinkedItems[] = {};
+		
+		backpack = "";
+	};
+	class AO_Taliban_RSquad_FRO :  {
+		_generalMacro = "AO_Taliban_RSquad_FRO";
+		author = $STR_LOP_FULL_NAME;
+		
+		scope = 2;
+		side = 1;
+	
+		faction = "AO_Taliban";
+		vehicleClass = "AO_Taliban_RSquad";
+		
+		displayName = "";
+		
+				uniformClass = "";
+		
+		Items[] = 	{};
+		RespawnItems[] = {};
+		
+		weapons[] = {};
+		respawnWeapons[] = {};
+		
+		magazines[] = 	{
+						};
+		respawnMagazines[] = {};
+		
+		linkedItems[] = {
+						};
+		respawnLinkedItems[] = {};
+		
+		backpack = "";
+	};
+	class AO_Taliban_RSquad_HM :  {
+		_generalMacro = "AO_Taliban_RSquad_HM";
+		author = $STR_LOP_FULL_NAME;
+		
+		scope = 2;
+		side = 1;
+	
+		faction = "AO_Taliban";
+		vehicleClass = "AO_Taliban_RSquad";
+		
+		displayName = "";
+		
+				uniformClass = "";
+		
+		Items[] = 	{};
+		RespawnItems[] = {};
+		
+		weapons[] = {};
+		respawnWeapons[] = {};
+		
+		magazines[] = 	{
+						};
+		respawnMagazines[] = {};
+		
+		linkedItems[] = {
+						};
+		respawnLinkedItems[] = {};
+		
+		backpack = "";
+	};
+	class AO_Taliban_RSquad_SL :  {
+		_generalMacro = "AO_Taliban_RSquad_SL";
+		author = $STR_LOP_FULL_NAME;
+		
+		scope = 2;
+		side = 1;
+	
+		faction = "AO_Taliban";
+		vehicleClass = "AO_Taliban_RSquad";
+		
+		displayName = "";
+		
+				uniformClass = "";
+		
+		Items[] = 	{};
+		RespawnItems[] = {};
+		
+		weapons[] = {};
+		respawnWeapons[] = {};
+		
+		magazines[] = 	{
+						};
+		respawnMagazines[] = {};
+		
+		linkedItems[] = {
+						};
+		respawnLinkedItems[] = {};
+		
+		backpack = "";
+	};
+	class AO_Taliban_RSquad_TL :  {
+		_generalMacro = "AO_Taliban_RSquad_TL";
+		author = $STR_LOP_FULL_NAME;
+		
+		scope = 2;
+		side = 1;
+	
+		faction = "AO_Taliban";
+		vehicleClass = "AO_Taliban_RSquad";
+		
+		displayName = "";
+		
+				uniformClass = "";
+		
+		Items[] = 	{};
+		RespawnItems[] = {};
+		
+		weapons[] = {};
+		respawnWeapons[] = {};
+		
+		magazines[] = 	{
+						};
+		respawnMagazines[] = {};
+		
+		linkedItems[] = {
+						};
+		respawnLinkedItems[] = {};
+		
+		backpack = "";
+	};
+	class AO_Taliban_RSquad_AR :  {
+		_generalMacro = "AO_Taliban_RSquad_AR";
+		author = $STR_LOP_FULL_NAME;
+		
+		scope = 2;
+		side = 1;
+	
+		faction = "AO_Taliban";
+		vehicleClass = "AO_Taliban_RSquad";
+		
+		displayName = "";
+		
+				uniformClass = "";
+		
+		Items[] = 	{};
+		RespawnItems[] = {};
+		
+		weapons[] = {};
+		respawnWeapons[] = {};
+		
+		magazines[] = 	{
+						};
+		respawnMagazines[] = {};
+		
+		linkedItems[] = {
+						};
+		respawnLinkedItems[] = {};
+		
+		backpack = "";
+	};
+	class AO_Taliban_RSquad_AAR :  {
+		_generalMacro = "AO_Taliban_RSquad_AAR";
+		author = $STR_LOP_FULL_NAME;
+		
+		scope = 2;
+		side = 1;
+	
+		faction = "AO_Taliban";
+		vehicleClass = "AO_Taliban_RSquad";
+		
+		displayName = "";
+		
+				uniformClass = "";
+		
+		Items[] = 	{};
+		RespawnItems[] = {};
+		
+		weapons[] = {};
+		respawnWeapons[] = {};
+		
+		magazines[] = 	{
+						};
+		respawnMagazines[] = {};
+		
+		linkedItems[] = {
+						};
+		respawnLinkedItems[] = {};
+		
+		backpack = "";
+	};
+	class AO_Taliban_RSquad_RO :  {
+		_generalMacro = "AO_Taliban_RSquad_RO";
+		author = $STR_LOP_FULL_NAME;
+		
+		scope = 2;
+		side = 1;
+	
+		faction = "AO_Taliban";
+		vehicleClass = "AO_Taliban_RSquad";
+		
+		displayName = "";
+		
+				uniformClass = "";
+		
+		Items[] = 	{};
+		RespawnItems[] = {};
+		
+		weapons[] = {};
+		respawnWeapons[] = {};
+		
+		magazines[] = 	{
+						};
+		respawnMagazines[] = {};
+		
+		linkedItems[] = {
+						};
+		respawnLinkedItems[] = {};
+		
+		backpack = "";
+	};
+	class AO_Taliban_RSquad_MM : rhsusf_usmc_marpat_d_rifleman {
+		_generalMacro = "AO_Taliban_RSquad_MM";
+		author = $STR_LOP_FULL_NAME;
+		
+		scope = 2;
+		side = 1;
+	
+		faction = "AO_Taliban";
+		vehicleClass = "AO_Taliban_RSquad";
+		
+		displayName = "Marksman";
+		
+				uniformClass = "";
+		
+		Items[] = 	{};
+		RespawnItems[] = {};
+		
+		weapons[] = {};
+		respawnWeapons[] = {};
+		
+		magazines[] = 	{
+						};
+		respawnMagazines[] = {};
+		
+		linkedItems[] = {
+						};
+		respawnLinkedItems[] = {};
+		
+		backpack = "";
+	};
+	class AO_Taliban_MGSquad_TL :  {
+		_generalMacro = "AO_Taliban_MGSquad_TL";
+		author = $STR_LOP_FULL_NAME;
+		
+		scope = 2;
+		side = 1;
+	
+		faction = "AO_Taliban";
+		vehicleClass = "AO_Taliban_MGSquad";
+		
+		displayName = "MG Team Leader";
+		
+				uniformClass = "";
+		
+		Items[] = 	{};
+		RespawnItems[] = {};
+		
+		weapons[] = {};
+		respawnWeapons[] = {};
+		
+		magazines[] = 	{
+						};
+		respawnMagazines[] = {};
+		
+		linkedItems[] = {
+						};
+		respawnLinkedItems[] = {};
+		
+		backpack = "";
+	};
+	class AO_Taliban_MGSquad_MG : rhsusf_usmc_marpat_d_machinegunner {
+		_generalMacro = "AO_Taliban_MGSquad_MG";
+		author = $STR_LOP_FULL_NAME;
+		
+		scope = 2;
+		side = 1;
+	
+		faction = "AO_Taliban";
+		vehicleClass = "AO_Taliban_MGSquad";
+		
+		displayName = "";
+		
+				uniformClass = "";
+		
+		Items[] = 	{};
+		RespawnItems[] = {};
+		
+		weapons[] = {};
+		respawnWeapons[] = {};
+		
+		magazines[] = 	{
+						};
+		respawnMagazines[] = {};
+		
+		linkedItems[] = {
+						};
+		respawnLinkedItems[] = {};
+		
+		backpack = "";
+	};
+	class AO_Taliban_MGSquad_AMG : rhsusf_usmc_marpat_d_machinegunner_ass {
+		_generalMacro = "AO_Taliban_RSquad_AMG";
+		author = $STR_LOP_FULL_NAME;
+		
+		scope = 2;
+		side = 1;
+	
+		faction = "AO_Taliban";
+		vehicleClass = "AO_Taliban_MGSquad";
+		
+		displayName = "";
+		
+				uniformClass = "";
+		
+		Items[] = 	{};
+		RespawnItems[] = {};
+		
+		weapons[] = {};
+		respawnWeapons[] = {};
+		
+		magazines[] = 	{
+						};
+		respawnMagazines[] = {};
+		
+		linkedItems[] = {
+						};
+		respawnLinkedItems[] = {};
+		
+		backpack = "";
+	};
